392ca88dbc9a4666e8610562915711a6e0cdbc39
apatwar <apatwar@ncsu.edu>
2015-06-01 17:41:40 -0400

    Test

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -2,8 +2,10 @@ class Devise::InvitationsController < DeviseController
 
   prepend_before_filter :authenticate_inviter!, :only => [:new, :create]
   prepend_before_filter :has_invitations_left?, :only => [:create]
-  prepend_before_filter :require_no_authentication, :only => [:edit, :update, :destroy]
-  prepend_before_filter :resource_from_invitation_token, :only => [:edit, :destroy]
+  prepend_before_filter :require_no_authentication, :only => [:edit, :update, :d
+estroy]
+  prepend_before_filter :resource_from_invitation_token, :only => [:edit, :destr
+oy]
   helper_method :after_sign_in_path_for
 
   # GET /resource/invitation/new
@@ -15,15 +17,12 @@ class Devise::InvitationsController < DeviseController
   # POST /resource/invitation
   def create
     self.resource = invite_resource
-    resource_invited = resource.errors.empty?
 
-    yield resource if block_given?
-
-    if resource_invited
-      if is_flashing_format? && self.resource.invitation_sent_at
-        set_flash_message :notice, :send_instructions, :email => self.resource.email
-      end
-      respond_with resource, :location => after_invite_path_for(current_inviter)
+    if resource.errors.empty?
+      yield resource if block_given?
+      set_flash_message :notice, :send_instructions, :email => self.resource.ema
+il if self.resource.invitation_sent_at
+      respond_with resource, :location => after_invite_path_for(resource)
     else
       respond_with_navigational(resource) { render :new }
     end
@@ -38,20 +37,18 @@ class Devise::InvitationsController < DeviseController
   # PUT /resource/invitation
   def update
     self.resource = accept_resource
-    invitation_accepted = resource.errors.empty?
 
-    yield resource if block_given?
+    if resource.errors.empty?
+      yield resource if block_given?
+       flash_message = resource.active_for_authentication? ? :updated : :updated
+_not_active
 
-    if invitation_accepted
-      if Devise.allow_insecure_sign_in_after_accept
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message :notice, flash_message if is_flashing_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_accept_path_for(resource)
-      else
-        set_flash_message :notice, :updated_not_active if is_flashing_format?
-        respond_with resource, :location => new_session_path(resource_name)
-      end
+ #params[:avatar]=session[:linkedin_signup_picture]
+
+    resource.avatar=session[:linkedin_signup_picture]
+      set_flash_message :notice, flash_message
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_accept_path_for(resource)
     else
       respond_with_navigational(resource){ render :edit }
     end
@@ -60,7 +57,7 @@ class Devise::InvitationsController < DeviseController
   # GET /resource/invitation/remove?invitation_token=abcdef
   def destroy
     resource.destroy
-    set_flash_message :notice, :invitation_removed if is_flashing_format?
+    set_flash_message :notice, :invitation_removed
     redirect_to after_sign_out_path_for(resource_name)
   end
 
@@ -81,14 +78,15 @@ class Devise::InvitationsController < DeviseController
   def has_invitations_left?
     unless current_inviter.nil? || current_inviter.has_invitations_left?
       self.resource = resource_class.new
-      set_flash_message :alert, :no_invitations_remaining if is_flashing_format?
+      set_flash_message :alert, :no_invitations_remaining
       respond_with_navigational(resource) { render :new }
     end
   end
 
   def resource_from_invitation_token
-    unless params[:invitation_token] && self.resource = resource_class.find_by_invitation_token(params[:invitation_token], true)
-      set_flash_message(:alert, :invitation_token_invalid) if is_flashing_format?
+    unless params[:invitation_token] && self.resource = resource_class.find_by_i
+nvitation_token(params[:invitation_token], true)
+      set_flash_message(:alert, :invitation_token_invalid)
       redirect_to after_sign_out_path_for(resource_name)
     end
   end
